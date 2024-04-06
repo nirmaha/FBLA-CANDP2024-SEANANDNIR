ed5366462bbd633f0f49a227180a2f86c2a3b337
ChaosCow58 <sryan@tryan.net>
2024-04-06 16:43:51 -0500

    Fixed some imports

--- MVVM/View/Pages/AddPartners.xaml.cs
+++ MVVM/View/Pages/AddPartners.xaml.cs
@@ -1,22 +1,15 @@
-﻿using EduPartners.Core;
-using EduPartners.MVVM.Model;
-using EduPartners.MVVM.View.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+
+using EduPartners.Core;
+using EduPartners.MVVM.Model;
+using EduPartners.MVVM.View.Controls;
 
 namespace EduPartners.MVVM.View.Pages
 {
