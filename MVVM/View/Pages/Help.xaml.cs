97d51883dd7522ed527712a6002812b5cfe2a230
nirmaha <86540335+nirmaha@users.noreply.github.com>
2024-04-14 14:38:26 -0500

    Added Interactive Help Page

--- MVVM/View/Pages/Help.xaml.cs
+++ MVVM/View/Pages/Help.xaml.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Input;
+
+using EduPartners.MVVM.View.Controls;
 
 
 namespace EduPartners.MVVM.View.Pages
@@ -14,6 +17,36 @@ namespace EduPartners.MVVM.View.Pages
         public Help()
         {
             InitializeComponent();
+        }
+
+        private void lDashboard_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainControl mainControl = App.Current.Properties["MainControl"] as MainControl;
+            mainControl.Load_Page("MVVM/View/Pages/Dashboard.xaml");
+        }
+
+        private void lViewPartners_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainControl mainControl = App.Current.Properties["MainControl"] as MainControl;
+            mainControl.Load_Page("MVVM/View/Pages/ViewPartners.xaml");
+        }
+
+        private void lAddPartners_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainControl mainControl = App.Current.Properties["MainControl"] as MainControl;
+            mainControl.Load_Page("MVVM/View/Pages/AddPartners.xaml");
+        }
+
+        private void lEditPartners_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainControl mainControl = App.Current.Properties["MainControl"] as MainControl;
+            mainControl.Load_Page("MVVM/View/Pages/ViewPartners.xaml");
+        }
+
+        private void lProfile_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            MainControl mainControl = App.Current.Properties["MainControl"] as MainControl;
+            mainControl.Load_Page("MVVM/View/Pages/Profile.xaml");
         }
     }
 }
