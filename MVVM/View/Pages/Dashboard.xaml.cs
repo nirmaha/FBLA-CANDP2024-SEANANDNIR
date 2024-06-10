14b36e3d0e1daaa3da32ce774c7feffe01826299
ChaosCow58 <sryan@tryan.net>
2024-06-10 18:57:07 -0500

    Upadated line graph

--- MVVM/View/Pages/Dashboard.xaml.cs
+++ MVVM/View/Pages/Dashboard.xaml.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Windows.Controls;
-using System.Windows.Controls.DataVisualization.Charting;
+
+using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
 
 using EduPartners.Core;
 using EduPartners.MVVM.Model;
@@ -15,9 +18,8 @@ namespace EduPartners.MVVM.View.Pages
     public partial class Dashboard : Page
     {
         private Database db;
-        
+
         private Dictionary<string, int> IndustryPopulation = new Dictionary<string, int>();
-        private Dictionary<int, int> YearToParnters = new Dictionary<int, int>();
         private Dictionary<string, double> IndustryToSavings = new Dictionary<string, double>();
 
         public Dashboard()
@@ -32,27 +34,23 @@ namespace EduPartners.MVVM.View.Pages
                 List<Partner> partners = school.Partners.Value;
                 partners = partners.OrderBy(p => p.StartDate.Year).ToList();
 
+                List<int> dates = new List<int>();
+                List<string> industries = new List<string>();
+
                 // Populates Dashboard Graphs
                 foreach (Partner partner in partners) 
                 {
+                    dates.Add(partner.StartDate.Year);
+                    industries.Add(partner.Industry);
+
                     // Industry Pie Chart
                     if (!IndustryPopulation.ContainsKey(partner.Industry))
                     {
                         IndustryPopulation.Add(partner.Industry, 1);
                     }
                     else
-                    { 
+                    {
                         IndustryPopulation[partner.Industry]++;
-                    }
-
-                    // Line Graph
-                    if (!YearToParnters.ContainsKey(partner.StartDate.Year))
-                    {
-                        YearToParnters.Add(partner.StartDate.Year, 1);
-                    }
-                    else
-                    {
-                        YearToParnters[partner.StartDate.Year]++;
                     }
 
                     // Industry Savings Pie Chart
@@ -67,10 +65,43 @@ namespace EduPartners.MVVM.View.Pages
 
                 }
 
-                ((PieSeries)pieIndustry.Series[0]).ItemsSource = IndustryPopulation;
-                ((LineSeries)lineNumofPartners.Series[0]).ItemsSource = YearToParnters;
-                ((PieSeries)pieIndustrySavings.Series[0]).ItemsSource = IndustryToSavings;
-                
+                // Line Graph
+                Dictionary<int, int> yearCounts = dates
+                          .GroupBy(year => year)
+                          .ToDictionary(group => group.Key, group => group.Count());
+
+                ChartValues<ObservablePoint> values = new ChartValues<ObservablePoint>();
+
+                foreach (KeyValuePair<int, int> year in yearCounts)
+                {
+                    values.Add(new ObservablePoint(year.Key, year.Value));
+                }
+
+
+                LineSeries lineSeries = new LineSeries()
+                {
+                    Title = "Partners",
+                    Values = values,
+                    PointGeometry = DefaultGeometries.Circle,
+                    
+                };
+
+                lineNumofPartners.Series = new SeriesCollection() { lineSeries };
+
+                // Industry Pie Chart
+                Dictionary<string, int> industryCount = industries
+                          .GroupBy(industry => industry)
+                          .ToDictionary(group => group.Key, group => group.Count());
+
+                SeriesCollection pieSeries = new SeriesCollection();
+
+                foreach (KeyValuePair<string, int> industry in industryCount)
+                {
+                    pieSeries.Add(new PieSeries() { Title = industry.Key, Values = new ChartValues<int> { industry.Value } });
+                }
+
+                pieIndustry.Series = pieSeries;
+
             };
         }
     }
