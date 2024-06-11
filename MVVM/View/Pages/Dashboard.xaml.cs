5a1ae2278e2d90c0abdd7a3600ba06c4b8e43330
ChaosCow58 <sryan@tryan.net>
2024-06-10 23:34:50 -0500

    Refactored graphs and updated libaries

--- MVVM/View/Pages/Dashboard.xaml.cs
+++ MVVM/View/Pages/Dashboard.xaml.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Controls;
+using System.Windows.Media;
 
 using LiveCharts;
 using LiveCharts.Defaults;
@@ -12,21 +14,33 @@ using EduPartners.MVVM.Model;
 
 namespace EduPartners.MVVM.View.Pages
 {
+    public class CustomLegendItem
+    {
+        public string Title { get; set; }
+        public Brush Color { get; set; }
+    }
+
     /// <summary>
     /// Interaction Logic for Dashboard.xaml
     /// </summary>
     public partial class Dashboard : Page
     {
         private Database db;
+        private Random rnd = new Random();
 
-        private Dictionary<string, int> IndustryPopulation = new Dictionary<string, int>();
+        public ObservableCollection<CustomLegendItem> LegendItems { get; set; }
+
         private Dictionary<string, double> IndustryToSavings = new Dictionary<string, double>();
+
+        public string[] BarChartLabels { get; set; }
+        public Func<double, string> CurrencyFormatter { get; set; }
 
         public Dashboard()
         {
             InitializeComponent();
             db = App.Current.Properties["Database"] as Database;
-            
+            DataContext = this;
+
             this.Loaded += async (sender, e) =>
             {
                 School school = (await db.GetSchoolById(App.Current.Properties["CurrentSchoolId"].ToString())).FirstOrDefault();
@@ -42,16 +56,6 @@ namespace EduPartners.MVVM.View.Pages
                 {
                     dates.Add(partner.StartDate.Year);
                     industries.Add(partner.Industry);
-
-                    // Industry Pie Chart
-                    if (!IndustryPopulation.ContainsKey(partner.Industry))
-                    {
-                        IndustryPopulation.Add(partner.Industry, 1);
-                    }
-                    else
-                    {
-                        IndustryPopulation[partner.Industry]++;
-                    }
 
                     // Industry Savings Pie Chart
                     if (!IndustryToSavings.ContainsKey(partner.Industry))
@@ -97,12 +101,67 @@ namespace EduPartners.MVVM.View.Pages
 
                 foreach (KeyValuePair<string, int> industry in industryCount)
                 {
-                    pieSeries.Add(new PieSeries() { Title = industry.Key, Values = new ChartValues<int> { industry.Value } });
+                    SolidColorBrush randomColor = new SolidColorBrush(Color.FromRgb((byte)rnd.Next(256), (byte)rnd.Next(256), (byte)rnd.Next(256)));
+
+                    pieSeries.Add
+                    (
+                        new PieSeries() 
+                        { 
+                            Title = industry.Key, 
+                            Values = new ChartValues<int> { industry.Value },
+                            Fill = randomColor
+                        }
+                    );
+                }
+
+                LegendItems = new ObservableCollection<CustomLegendItem>();
+                foreach (Series series in pieSeries)
+                {
+                    LegendItems.Add(new CustomLegendItem { Title = series.Title, Color = series.Fill });
                 }
 
                 pieIndustry.Series = pieSeries;
+                legendListBox.ItemsSource = LegendItems;
 
+                ChartValues<double> savings = new ChartValues<double>();
+
+                foreach (double saving in IndustryToSavings.Values)
+                {
+                    savings.Add(saving);
+                }
+
+                // Bar Graph
+                ColumnSeries columnSeries = new ColumnSeries()
+                {
+                    Title = "Partners",
+                    Values = savings,
+                };
+
+                List<string> listLabels = new List<string>();
+                foreach (string industry in IndustryToSavings.Keys)
+                { 
+                    listLabels.Add(industry);
+                }
+
+                BarChartLabels = listLabels.ToArray();
+                CurrencyFormatter = value => value.ToString("C");
+
+                SeriesCollection barChartCollection = new SeriesCollection() { columnSeries };
+                barIndustrySavings.Series = barChartCollection;
             };
+        }
+
+        private void PieChart_DataClick(object sender, ChartPoint chartPoint)
+        {
+            PieChart chart = (PieChart)chartPoint.ChartView;
+
+            foreach (PieSeries series in chart.Series)
+            {
+                series.PushOut = 0;
+            }
+
+            PieSeries selectedSeries = (PieSeries)chartPoint.SeriesView;
+            selectedSeries.PushOut = 8;
         }
     }
 }
